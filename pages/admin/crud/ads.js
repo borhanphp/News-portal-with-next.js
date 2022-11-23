66a5e02613d7e6ede261d9f112df6ccfc0ce7d09
Borhan Uddin <78135643+borhanphp@users.noreply.github.com>
2022-11-23 15:35:37 +0600

    Update ads.js

--- pages/admin/crud/ads.js
+++ pages/admin/crud/ads.js
@@ -1,34 +1,11 @@
 import React from 'react'
-import Layout from '../../../components/Layout';
-import Admin from '../../../components/auth/Admin';
-import Ads from '../../../components/crud/Ads'
-import StyleLinks from '../../../components/StyleLinks';
-import Topbanner from '../../../components/crud/ads/Topbanner';
-import HomeRightAds from '../../../components/crud/ads/HomeRightAds';
-import Homemiddlebigads from '../../../components/crud/ads/Homemiddlebigads';
-import Homemiddlefirst from '../../../components/crud/ads/Homemiddlefirst';
-import Homemiddlesec from '../../../components/crud/ads/Homemiddlesec';
-import Belowpostads from '../../../components/crud/ads/Belowpostads';
+
 
 
 
 const AdminIndex = () => {
   return (
     <>
-
-  
-    
-    <Layout>
-      <Admin>
-        <Topbanner/>
-        <HomeRightAds/>
-        <Homemiddlebigads/>
-        <Homemiddlefirst />
-        <Homemiddlesec/>
-        <Belowpostads/>
-      </Admin>
-    </Layout>
-    <StyleLinks/>
     </>
 
   )
