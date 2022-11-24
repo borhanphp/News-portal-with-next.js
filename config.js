2318c774f1b0b33df2c22eb8ecf209c00ebe0799
Borhan Uddin <78135643+borhanphp@users.noreply.github.com>
2022-11-24 18:34:25 +0600

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ export const API = 'https://api-mliu.onrender.com/api';
 export const DOMAIN_IP = 'http://bdtech.live';
 export const IMG_API = 'http://api-mliu.onrender.com/api/images';
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
-export const DOMAIN = "chalamannewyork.com";
+export const DOMAIN = "bdtech.live";
 
 export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
 // publicRuntimeConfig.PRODUCTION 
