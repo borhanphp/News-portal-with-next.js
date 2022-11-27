f8d89a45437bfc9fe6cef236575ba8f4a4d4cd34
Borhan Uddin <78135643+borhanphp@users.noreply.github.com>
2022-11-27 11:04:47 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,7 @@
 import getConfig from 'next/config'
 const {publicRuntimeConfig} = getConfig()
 
-<<<<<<< HEAD
-export const API = 'localhost:5000/api';
-export const DOMAIN_IP = 'http://82.180.161.124';
-export const IMG_API = 'https://82.180.161.124/api/images';
-=======
+
 export const API = 'https://api-mliu.onrender.com/api';
 export const DOMAIN_IP = 'http://bdtech.live';
 export const IMG_API = 'http://api-mliu.onrender.com/api/images';
