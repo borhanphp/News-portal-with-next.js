b66688c7f9d816ef28be3e885365ab8c4c7e3744
Borhan Uddin <78135643+borhanphp@users.noreply.github.com>
2023-03-05 05:04:10 +0600

    render api

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 import getConfig from 'next/config'
 const {publicRuntimeConfig} = getConfig()
 
-export const API = 'https://frightened-lime-llama.cyclic.app/api';
+export const API = 'https://bdtechlive.onrender.com/api';
 export const DOMAIN_IP = 'http://82.180.161.124';
-export const IMG_API = 'https://frightened-lime-llama.cyclic.app/api/images';
+export const IMG_API = 'https://bdtechlive.onrender.com/api/images';
 export const APP_NAME = publicRuntimeConfig.APP_NAME;
 export const DOMAIN = "digitalnews.com";
 export const OPEN_API_KEY = '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';
